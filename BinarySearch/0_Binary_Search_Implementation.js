6166b469bcd2c668f0a8f443bb8a4e2ce03ea8da
Md.Asif Parvez Sarker <AsifParvezsarker26@gmail.com>
2023-09-10 09:46:13 +0600

    Binary Search Implementation done

--- BinarySearch/0_Binary_Search_Implementation.js
+++ BinarySearch/0_Binary_Search_Implementation.js
@@ -1,1 +1,23 @@
-console.log("hi")
+function binarySearch(arr, target){
+    let start = 0;
+    let end = arr.length -1;
+    let mid;
+
+    while(start <= end ){
+        mid = Math.floor((start + end) / 2);
+
+        if(arr[mid] === target){
+            return true;
+        }else if(arr[mid] < target){
+            start = mid + 1;
+        }else{
+            end = mid -1;
+        }
+    }
+
+    return false;
+}
+
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10], 10))
+
+/* Time Complexity is O(logN) */
